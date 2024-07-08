f6970029543b09680794386936376f8f89556d86
tahaasann <tahaasann@gmail.com>
2024-07-08 10:55:00 +0300

    Sayılar ve Karakter dizilerine giriş ile başlandı

---
+++ learn-python.py
@@ -0,0 +1,16 @@
+# istatistik açısından (sayısal değişken)
+9 #integer - kesikli
+9.2 #float - sürekli
+9+9
+9*9
+
+# stringler (karakter dizileri)
+# String olduğu bilgisini vermek için " - çift tırnak veya ' - tek tırnak kullanılmalıdır.
+print('HELLO AI ERA') # İçine yazdığımız ifadeyi konsol ekranına yazdırır.
+"HELLO AI ERA"
+
+# python programlama dilinde oluşturduğumuz her şeye nesne adı verilir(nesne yönelimli programlama prensipleri). 
+
+type(9) # herhangi bir nesnenin tipini belirlemek için
+type(9.2)
+type("HELLO AI ERA")
